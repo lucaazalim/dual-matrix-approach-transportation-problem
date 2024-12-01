f5314fc61ee910cd1fae1cece0f3555bef1ea82a
Luca Azalim <lucaazalim@gmail.com>
2024-12-01 16:02:17 -0300

    Still working on the implementation.

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,22 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * This Java implementation demonstrates the Dual-Matrix Approach for solving
+ * the Transportation Problem, as proposed by Ping Ji and K.F. Chu in their
+ * paper, "A Dual-Matrix Approach to the Transportation Problem," published in
+ * the Asia-Pacific Journal of Operational Research, Volume 19, 2002, pages 35-45.
+ *
+ * @author Luca Azalim
+ */
+
 public class Main {
 
-    static int supplyCountM, demandCountN;
-    static int[] supplyValues, demandValues;
-    static int[][] costValues;
+    static int[] supply, demand, A, Y, Q, P, v, u, vIndex;
+    static int[][] costs, gama, theta, D;
+    static int m, n, iteration, dimension, psi, k = 0, s = 0, t = 0;
 
-    static int[] A;
-    static int[][] gama;
-    static int[] u;
-    static int[] v;
-    static int[] vIndex;
-    static int[][] D;
-    static int dimension;
-    static int psi; // ψ
+    static String ANSI_RESET = "\033[0m", ANSI_YELLOW = "\033[33m", ANSI_GREEN = "\033[32m";
 
     public static void main(String[] args) throws IOException {
 
@@ -27,16 +29,24 @@ public class Main {
         initialize();
         iterate();
 
+        System.out.println(ANSI_GREEN + "# Total transportation cost: " + psi);
+
+        for (int i = 0; i < gama.length; i++) {
+            System.out.println("# Route " + Arrays.toString(gama[i]) + " -> " + Y[i]);
+        }
+
+        System.out.print(ANSI_RESET);
+
     }
 
     static void readInput() throws IOException {
 
         try (Scanner scanner = new Scanner(System.in)) {
 
-            System.out.println("Enter the file name: ");
+            System.out.println("Enter the input file name: ");
             String fileName = scanner.nextLine();
 
-            Path path = Path.of(fileName);
+            Path path = Path.of("input", fileName);
 
             if (Files.notExists(path)) {
                 throw new IllegalArgumentException("File not found: " + fileName);
@@ -46,27 +56,27 @@ public class Main {
 
                 String[] countLine = reader.readLine().split("\\s+");
 
-                supplyCountM = Integer.parseInt(countLine[0]);
-                demandCountN = Integer.parseInt(countLine[1]);
+                m = Integer.parseInt(countLine[0]);
+                n = Integer.parseInt(countLine[1]);
 
-                supplyValues = new int[supplyCountM];
-                demandValues = new int[demandCountN];
-                costValues = new int[supplyCountM][demandCountN];
+                supply = new int[m];
+                demand = new int[n];
+                costs = new int[m][n];
 
-                for (int i = 0; i < supplyCountM; i++) {
-                    supplyValues[i] = Integer.parseInt(reader.readLine());
+                for (int i = 0; i < m; i++) {
+                    supply[i] = Integer.parseInt(reader.readLine());
                 }
 
-                for (int i = 0; i < demandCountN; i++) {
-                    demandValues[i] = Integer.parseInt(reader.readLine());
+                for (int i = 0; i < n; i++) {
+                    demand[i] = Integer.parseInt(reader.readLine());
                 }
 
-                for (int i = 0; i < supplyCountM; i++) {
+                for (int i = 0; i < m; i++) {
 
                     String[] costLine = reader.readLine().split("\\s+");
 
-                    for (int j = 0; j < demandCountN; j++) {
-                        costValues[i][j] = Integer.parseInt(costLine[j]);
+                    for (int j = 0; j < n; j++) {
+                        costs[i][j] = Integer.parseInt(costLine[j]);
                     }
 
                 }
@@ -79,25 +89,27 @@ public class Main {
 
     static void initialize() {
 
+        System.out.println(ANSI_YELLOW + "Step 0" + ANSI_RESET);
+
         // Step 0.1
 
-        A = new int[supplyCountM + demandCountN];
+        A = new int[m + n];
 
-        System.arraycopy(demandValues, 0, A, 0, demandCountN);
+        System.arraycopy(demand, 0, A, 0, n);
 
-        for (int i = 0; i < supplyCountM; i++) {
-            A[demandCountN + i] = supplyValues[i] * -1;
+        for (int i = 0; i < m; i++) {
+            A[n + i] = supply[i] * -1;
         }
 
         System.out.println("A = " + Arrays.toString(A));
 
         // Step 0.2 and 0.3
 
-        gama = new int[supplyCountM + demandCountN][2];
+        gama = new int[m + n][2];
 
-        u = new int[supplyCountM];
-        v = new int[demandCountN];
-        vIndex = new int[demandCountN];
+        u = new int[m];
+        v = new int[n];
+        vIndex = new int[n];
 
         Arrays.fill(u, 0);
 
@@ -105,41 +117,41 @@ public class Main {
 
         Arrays.fill(v, Integer.MAX_VALUE);
 
-        for (int i = 0; i < demandCountN; i++) {
-            for (int j = 0; j < supplyCountM; j++) {
-                if (costValues[j][i] < v[i]) {
-                    v[i] = costValues[j][i];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (costs[j][i] < v[i]) {
+                    v[i] = costs[j][i];
                     vIndex[i] = j;
                     gama[i] = new int[]{j, i};
                 }
+
             }
         }
 
         System.out.println("v = " + Arrays.toString(v));
         System.out.println("vIndex = " + Arrays.toString(vIndex));
 
-        for (int i = 0; i < supplyCountM; i++) {
-            gama[demandCountN + i] = new int[]{i, -1};
+        for (int i = 0; i < m; i++) {
+            gama[n + i] = new int[]{i, -1};
         }
 
         System.out.println("Γ = " + Arrays.deepToString(gama));
 
         // Step 0.4
 
-        dimension = supplyCountM + demandCountN;
+        dimension = m + n;
 
         D = new int[dimension][dimension];
 
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
 
-                if (i == j && i < demandCountN) {
+                if (i == j && i < n) {
                     D[i][j] = 1;
-                } else if (i < demandCountN
-                        && j >= demandCountN
-                        && j == demandCountN + vIndex[i]) {
+                } else if (i < n && j >= n && j == n + vIndex[i]) {
                     D[i][j] = -1;
-                } else if (i == j && j >= demandCountN) {
+                } else if (i == j) {
                     D[i][j] = -1;
                 }
 
@@ -149,8 +161,6 @@ public class Main {
         System.out.println("D = ");
         printMatrix(D);
 
-        // Computing the objective (ψ)
-
         psi = computeObjective(v, u);
 
         System.out.println("ψ = " + psi);
@@ -159,17 +169,17 @@ public class Main {
 
     static void iterate() {
 
-        System.out.println("# Iterating...");
+        System.out.println(ANSI_YELLOW + "# Step 1..3 (x" + ++iteration + ")" + ANSI_RESET);
 
         // Step 1.1
 
-        int[] Y = dotProduct(A, D);
+        Y = dotProduct(A, D);
 
         System.out.println("Y = " + Arrays.toString(Y));
 
         // Step 1.2
 
-        int k = indexOfMinValue(Y);
+        k = indexOfMinValue(Y);
 
         System.out.println("k = " + k);
 
@@ -182,40 +192,35 @@ public class Main {
 
         // Step 2.1
 
-        int[] Q = new int[demandCountN];
+        Q = new int[n];
 
-        for (int i = 0; i < demandCountN; i++) {
+        for (int i = 0; i < n; i++) {
             Q[i] = D[i][k];
         }
 
         System.out.println("Q = " + Arrays.toString(Q));
 
-        int[] P = new int[supplyCountM];
+        P = new int[m];
 
-        for (int j = 0; j < supplyCountM; j++) {
-            P[j] = D[demandCountN + j][k];
+        for (int j = 0; j < m; j++) {
+            P[j] = D[n + j][k];
         }
 
         System.out.println("P = " + Arrays.toString(P));
 
         // Step 2.2
 
-        int[][] theta = new int[supplyCountM][demandCountN];
+        theta = new int[m][n];
         int minTheta = Integer.MAX_VALUE;
-        int s = 0, t = 0;
 
-        for (int i = 0; i < supplyCountM; i++) {
-            for (int j = 0; j < demandCountN; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-                if (D[i][j] != 0) {
+                if (D[i][j] != 0 || P[i] - Q[j] <= 0) {
                     continue;
                 }
 
-                if (P[i] - Q[j] <= 0) {
-                    continue;
-                }
-
-                theta[i][j] = costValues[i][j] + u[i] - v[j];
+                theta[i][j] = costs[i][j] + u[i] - v[j];
 
                 // Step 2.3
 
@@ -235,21 +240,31 @@ public class Main {
         System.out.println("t = " + t);
         System.out.println("Θst = " + theta[s][t]);
 
+        update();
+
+        iterate();
+
+    }
+
+    static void update() {
+
         // Step 3.1
 
+        // Step 3.1.1
+
         for (int l = 0; l < dimension; l++) {
-
-            // Step 3.1.1
-
             D[l][k] *= -1;
+        }
 
-            // Step 3.1.2
+        // Step 3.1.2
 
+        for (int l = 0; l < dimension; l++) {
             for (int r = 0; r < dimension; r++) {
-                if (r == k) continue;
-                D[l][r] += (D[s + demandCountN][r] - D[t][r]) * D[l][k];
-            }
 
+                if (r == k) continue;
+                D[l][r] += (D[s + n][r] - D[t][r]) * D[l][k];
+
+            }
         }
 
         System.out.println("D = ");
@@ -263,19 +278,21 @@ public class Main {
 
         // Step 3.3
 
-        for (int i = 0; i < supplyCountM; i++) {
+        for (int i = 0; i < m; i++) {
             u[i] = u[i] - theta[s][t] * P[i];
         }
 
-        for (int j = 0; j < demandCountN; j++) {
+        System.out.println("u = " + Arrays.toString(u));
+
+        for (int j = 0; j < n; j++) {
             v[j] = v[j] - theta[s][t] * Q[j];
         }
+
+        System.out.println("v = " + Arrays.toString(v));
 
         psi = computeObjective(v, u);
 
         System.out.println("ψ = " + psi);
-
-        iterate();
 
     }
 
@@ -283,14 +300,14 @@ public class Main {
 
         int firstSummation = 0;
 
-        for (int j = 0; j < demandCountN; j++) {
-            firstSummation += demandValues[j] * v[j];
+        for (int j = 0; j < n; j++) {
+            firstSummation += demand[j] * v[j];
         }
 
         int secondSummation = 0;
 
-        for (int i = 0; i < supplyCountM; i++) {
-            secondSummation += supplyValues[i] * u[i];
+        for (int i = 0; i < m; i++) {
+            secondSummation += supply[i] * u[i];
         }
 
         return firstSummation - secondSummation;
